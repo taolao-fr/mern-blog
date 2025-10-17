b4118467dbd4a4fa1568f3a313f4b819511b94b8
Duy Nguyen <duynguyen212@gmail.com>
2025-10-17 16:55:53 +0200

    Connecting to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';    
 
 const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+    .then(()=>{
+        console.log('Connected to MongoDB');
+    }).catch((err)=>{
+        console.error('Failed to connect to MongoDB', err);
+    });
+
 
 app.listen(3000, () => {
     console.log('API server is running on http://localhost:3000');
